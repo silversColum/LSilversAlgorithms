a5c857170d74b9abab9a6316ebe5063f4c4321bf
Silvers <Silvers@916-IAM-139-05>
2024-10-28 19:12:08 -0500

    Add project files.

---
+++ LSilversAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Connect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "LSilvers Connect 4 Game";
+            Game game = new Game();
+            game.Play();
+        }
+    }
+}
